531a659e388d62499b418738aa01176be35df006
Miki <161672000+Miki553@users.noreply.github.com>
2024-03-16 20:41:44 +0700

    Update upload.ts

--- api/upload.ts
+++ api/upload.ts
@@ -13,13 +13,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL,deleteObject } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDB0wentj8Igqan_Bpe7g-MOEvCSSiSnt0",
-    authDomain: "api-test-94f3b.firebaseapp.com",
-    projectId: "api-test-94f3b",
-    storageBucket: "api-test-94f3b.appspot.com",
-    messagingSenderId: "291742907529",
-    appId: "1:291742907529:web:ea2f1dc03bcbde0462da58",
-    measurementId: "G-P99T31SED3"
+  apiKey: "AIzaSyDB0wentj8Igqan_Bpe7g-MOEvCSSiSnt0",
+  authDomain: "api-test-94f3b.firebaseapp.com",
+  projectId: "api-test-94f3b",
+  storageBucket: "api-test-94f3b.appspot.com",
+  messagingSenderId: "291742907529",
+  appId: "1:291742907529:web:ea2f1dc03bcbde0462da58",
+  measurementId: "G-P99T31SED3"
 };
 
 
@@ -41,15 +41,19 @@ const fileupload = new FileMiddleware();
 
 //add
 router.post("/", fileupload.diskLoader.single("file"), async (req, res) => {
-    const filename = Math.round(Math.random() * 100000) + '.png';
-    const storageRef = ref(storage, "/images/" + filename);
-    const metadata = { contentType: req.file!.mimetype };
-    const snapshot = await uploadBytesResumable(storageRef, req.file!.buffer, metadata);
-    const url = await getDownloadURL(snapshot.ref);
-    res.status(200).json({
-        filename: filename,
-        url :url
-    });
+    if (req.file && req.file.mimetype) {
+        const filename = Math.round(Math.random() * 100000) + '.png';
+        const storageRef = ref(storage, "/images/" + filename);
+        const metadata = { contentType: req.file.mimetype };
+        const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+        const url = await getDownloadURL(snapshot.ref);
+        res.status(200).json({
+            filename: filename,
+            url: url
+        });
+    } else {
+        res.status(400).json({ error: 'No file uploaded or invalid file' });
+    }
 });
 
 
